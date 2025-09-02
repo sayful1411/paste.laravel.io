7ff7bd2eab61e82043a9d31519a00870cb9590f1
sayful <mdsayfulislam1411@gmail.com>
2025-09-02 15:45:23 +0600

    chore: install and configure torchlight package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Torchlight\Middleware\RenderTorchlight::class);
         $middleware->redirectGuestsTo(fn () => route('login'));
         $middleware->redirectUsersTo(AppServiceProvider::HOME);
 
